467317086125752981f10630ca0ee96165c2f9b6
Anthony Fabian <anthony.fabian88@gmail.com>
2020-06-21 20:57:14 -0400

    Update COVID-19 SIR Simulation.py

--- COVID-19 SIR Simulation.py
+++ COVID-19 SIR Simulation.py
@@ -5,5 +5,53 @@ import matplotlib.pyplot as plt
 GREY = (0.78, 0.78, 0.78)   # Suseptible
 RED = (0.96, 0.15, 0.15)    # Infected
 GREEN = (0, 0.86, 0.03)     # Recovered
-BLACK = (0, 0, 0)           # Deceased
+BLACK = (0, 0, 0)           # Dead
 
+COVID19_PARAMS = {
+    "r0": 2.28,
+    "incubation": 5,
+    "percent_mild": 0.8,
+    "percent_severe": 0.2,
+    "severe_recovery": (21, 42),
+    "severe_death": (14, 56),
+    "fatality_rate": 0.034
+    "serial_interval": 7
+}
+
+class Virus():
+
+    def __init__(self, params):
+        
+        # Create plot
+        self.fig = plt.figure()
+        self.axes = self.fig.add_subplot(111, projection="polar")
+        self.axes.grid(False)
+        self.axes.set_xticklabels([])
+        self.axes.set_yticklabels([])
+        self.axes.set_ylim(0, 1)
+
+        # Create annotations
+        self.day_text = self.axes.annotate(
+            "Day 0", xy=[np.pi / 2, 1], ha="center". va="bottom"
+        )
+        self.infected_text = self.axes.annotate(
+            "Infected: 0", xy=[3 * np.pi / 2, 1], ha="center". va="top", color=RED
+        )
+        self.deaths_text = self.axes.annotate(
+            "\nDeaths: 0", xy=[3 * np.pi / 2, 1], ha="center". va="top", color=BLACK
+        )
+        self.recovered_text = self.axes.annotate(
+            "\n\nRecovered: 0", xy=[3 * np.pi / 2, 1], ha="center". va="top", color=GREEN
+        )
+
+        # Create member variables
+        self.day = 0
+        self.total_num_infected = 0
+        self.num_currently_infected = 0
+        self.num_recovered = 0
+        self.num_deaths = 0
+        self.r0 = params["r0"]
+        self.percent_mild = params["percent_mild"]
+        self.percent_severe = params["percent_severe"]
+        self.fatality_rate = params["fatality_rate"]
+        self.serial_interval = params["serial_interval"]
