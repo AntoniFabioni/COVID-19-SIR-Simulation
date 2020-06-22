bf6a88bdf634925ab158af1ccff70263b87c91dd
Anthony Fabian <anthony.fabian88@gmail.com>
2020-06-21 20:37:16 -0400

    Create COVID-19 SIR Simulation.py

---
+++ COVID-19 SIR Simulation.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.animation as ani
+import matplotlib.pyplot as plt
+
+GREY = (0.78, 0.78, 0.78)   # Suseptible
+RED = (0.96, 0.15, 0.15)    # Infected
+GREEN = (0, 0.86, 0.03)     # Recovered
+BLACK = (0, 0, 0)           # Deceased
+
